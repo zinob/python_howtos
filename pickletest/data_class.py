c59f4c676ec80f17beaf38e82e6f50b38f3f3c93
Simon Albinsson <giunk@zxz.se>
2023-07-06 23:02:14 +0200

    Changes data_class from a dataclass
    In order to perserve the feeling of "Real Life Horrors" the dirty "dataclass"
    has been changed from dataclass to a more accurate "data class"

--- pickletest/data_class.py
+++ pickletest/data_class.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass
 import process_class
 
-@dataclass
 class host:
-    name: str
-    ip: int
+    def __init__(s,name,ip):
+        s.name = name
+        s.ip = ip
+
+    def __repr__(s):
+        return (f"name:{s.name} ip:{s.ip}")
     def change_ip(self):
         process_class.increase_ip(self)
         return self
